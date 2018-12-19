b8f299c3f80bfb9540e759761275136caf1a9c90
nuansengsy <nuansengsyy@gmail.com>
2018-12-20 02:47:15 +0300

    Initial commit

---
+++ app/src/main/java/com/nuansnegsy/calculator/model/ExpressionEvaluator.kt
@@ -0,0 +1,90 @@
+package com.nuansnegsy.calculator.model
+
+import com.nuansnegsy.calculator.util.runInBackground
+import com.nuansnegsy.calculator.util.runOnUI
+import com.udojava.evalex.Expression
+import java.math.BigDecimal
+
+object ExpressionEvaluator {
+
+    private var resultCallback: ResultCallback? = null
+    private val expressionItems: MutableList<Item> = mutableListOf()
+
+    fun setResultCallback(resultCallback: ResultCallback?) {
+        this.resultCallback = resultCallback
+    }
+
+    /**
+     * @return true if the item value has to be appended to the displaying view
+     */
+    fun process(item: Item): Boolean {
+        if (expressionItems.isNotEmpty() && expressionItems.last() == Item.DOT && item == Item.DOT) {
+            return false
+        }
+        if (!item.type.isAction()) {
+            expressionItems.add(item)
+        } else {
+            processAction(item)
+        }
+        return true
+    }
+
+    private fun processAction(item: Item) {
+        when (item) {
+            Item.EQUALS -> {
+                calculate()
+            }
+            Item.CLEAR -> {
+                clearExpressionItems()
+                resultCallback?.onClear()
+            }
+            Item.DELETE -> {
+                var removedItem: Item? = null
+                if (expressionItems.isNotEmpty()) {
+                    removedItem = expressionItems.removeAt(expressionItems.size - 1)
+                }
+                if (removedItem != null) {
+                    resultCallback?.onDelete(removedItem)
+                }
+            }
+            else -> {
+            }
+        }
+    }
+
+    private fun calculate() {
+        runInBackground {
+            val expressionString = StringBuilder()
+            expressionItems.forEach {
+                expressionString.append(it.value)
+            }
+            try {
+                val result = Expression(expressionString.toString()).eval()
+                clearExpressionItems()
+                expressionItems.add(Item(result.toPlainString(), Type.NUMBER))
+                runOnUI { resultCallback?.onSuccess(result) }
+            } catch (exception: Expression.ExpressionException) {
+                clearExpressionItems()
+                runOnUI { resultCallback?.onError(exception.localizedMessage) }
+            } catch (exception: ArithmeticException) {
+                clearExpressionItems()
+                runOnUI { resultCallback?.onError(exception.localizedMessage) }
+            } catch (exception: NumberFormatException) {
+                clearExpressionItems()
+                runOnUI { resultCallback?.onError("Invalid expression!") }
+            }
+        }
+    }
+
+    private fun clearExpressionItems() {
+        expressionItems.clear()
+    }
+
+    interface ResultCallback {
+        fun onSuccess(bigDecimal: BigDecimal)
+        fun onError(message: String)
+        fun onClear()
+        fun onDelete(item: Item)
+    }
+
+}
---
+++ app/src/main/java/com/nuansnegsy/calculator/model/Type.kt
@@ -0,0 +1,15 @@
+package com.nuansnegsy.calculator.model
+
+enum class Type(val code: Int) {
+
+    ACTION_EQUALS(1),
+    ACTION_DELETE(2),
+    ACTION_CLEAR(3),
+    ACTION_DEFAULT(4),
+    OPERATOR(5),
+    HELPER(6),
+    NUMBER(7);
+
+    fun isAction() = this.code <= 4
+
+}
